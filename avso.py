0e0267942481ee595b0dee45b74b26f42e76beae
Alfons Örtemyr <oral16@nti.umea>
2019-01-22 10:42:42 +0100

    second commit

--- avso.py
+++ avso.py
@@ -1,5 +1,10 @@
-import argparse, os
+import argparse, os,csv
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f",type=str ,help="increase output file verbosity")
 args = parser.parse_args()
 print(args.file)
+with open(args.file, newline='') as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+        #print(row['first_name'], row['last_name'])
+        print(row)
